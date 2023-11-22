ea2707f571b19087a11c2198660e00be3bbed5d5
Mantoizzz <641538994@qq.com>
2023-11-22 20:53:03 +0800

    config some table

--- src/main/java/com/forum/mantoi/sys/entity/Message.java
+++ src/main/java/com/forum/mantoi/sys/entity/Message.java
@@ -1,13 +1,25 @@
 package com.forum.mantoi.sys.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
 
 @Entity
+@Data
 public class Message {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private Long fromId;
 
+    private Long toId;
+
+    private String content;
+
+    private int status;
+
+    private Date createTime;
 }
---
+++ src/main/java/com/forum/mantoi/sys/entity/Topic.java
@@ -0,0 +1,18 @@
+package com.forum.mantoi.sys.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "t_topic")
+public class Topic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content")
+    private String content;
+
+
+}
